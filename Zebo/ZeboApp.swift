55764d0d47df3bdfd7db54c349da1e6c80e13ab4
NigoraMamad <nigoramamdv@gmail.com>
2025-06-10 22:38:10 +0200

    Initial Commit

---
+++ Zebo/ZeboApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZeboApp.swift
+//  Zebo
+//
+//  Created by Nigorakhon Mamadalieva on 10/06/25.
+//
+
+import SwiftUI
+
+@main
+struct ZeboApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
