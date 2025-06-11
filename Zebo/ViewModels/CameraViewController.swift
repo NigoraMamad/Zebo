32beddd322f7007528bec123ff381ffa41c96349
NigoraMamad <nigoramamdv@gmail.com>
2025-06-11 14:52:00 +0200

    Extracted the face region from camera frames

--- Zebo/ViewModels/CameraViewController.swift
+++ Zebo/ViewModels/CameraViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import AVFoundation
+import Vision
+import CoreImage
 
 class CameraViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
 
@@ -42,8 +44,49 @@ class CameraViewController: UIViewController, AVCaptureVideoDataOutputSampleBuff
         session.startRunning()
     }
 
-    // We'll handle each frame here later
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-        // Will add Vision code here in Step 2
+        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+
+        let request = VNDetectFaceRectanglesRequest { [weak self] request, error in
+            guard let observations = request.results as? [VNFaceObservation],
+                  let face = observations.first else { return }
+
+            self?.processFaceObservation(face, in: pixelBuffer)
+        }
+
+        let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, orientation: .leftMirrored)
+        try? imageRequestHandler.perform([request])
     }
+    
+    func processFaceObservation(_ face: VNFaceObservation, in pixelBuffer: CVPixelBuffer) {
+        let ciImage = CIImage(cvPixelBuffer: pixelBuffer)
+
+        // Convert bounding box (normalized) to pixel coordinates
+        let width = CGFloat(CVPixelBufferGetWidth(pixelBuffer))
+        let height = CGFloat(CVPixelBufferGetHeight(pixelBuffer))
+
+        let faceRect = CGRect(
+            x: face.boundingBox.origin.x * width,
+            y: (1 - face.boundingBox.origin.y - face.boundingBox.size.height) * height,
+            width: face.boundingBox.size.width * width,
+            height: face.boundingBox.size.height * height
+        )
+
+        let cropped = ciImage.cropped(to: faceRect)
+        analyzeSkinColor(in: cropped)
+    }
+    
+    func analyzeSkinColor(in image: CIImage) {
+        let context = CIContext()
+        guard let cgImage = context.createCGImage(image, from: image.extent) else { return }
+
+        let uiImage = UIImage(cgImage: cgImage)
+        guard let averageColor = uiImage.averageColor else { return }
+
+        DispatchQueue.main.async {
+            print("🎨 Skin color: \(averageColor)")
+            // Next: classify this color into a season
+        }
+    }
+
 }
